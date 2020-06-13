3b8de9e9554c584d6f8b8569b84bfa22149df172
junners <jose.junryan@gmail.com>
2020-06-14 04:13:16 +0800

    implemented save and update backend functions

--- src/server/pg-ext/query.js
+++ src/server/pg-ext/query.js
@@ -6,9 +6,56 @@ const select_query = `SELECT * FROM ${tableName}`;
 
 exports.doQuery = (callback) => {
   pgdb(async (client) => {
-    // once connected we query
-    const res = await client.query(select_query);
-    console.log(res);
-    callback(null, res);
+    // eslint-disable-next-line no-use-before-define
+    await new PgAction(select_query, client).doAction(callback);
   });
 };
+
+const insert = (callback, ...parameters) => {
+  const modified = parameters.map(
+    (instance) => `('${instance.title}', '${instance.description}')`
+  );
+  const insert_statement = `INSERT INTO ${tableName}(title, content) VALUES ${modified.join(
+    ","
+  )} RETURNING *`;
+  pgdb(async (client) => {
+    // eslint-disable-next-line no-use-before-define
+    await new PgAction(insert_statement, client).doAction(callback);
+  });
+};
+
+const update = (callback, param) => {
+  const id = param.id;
+  const modified_param = { title: param.title, content: param.description };
+  const formatted = Object.fromEntries(
+    Object.entries(modified_param).map(([key, value]) => `${key}='${value}'`)
+  );
+  const update_statement = `UPDATE ${tableName} SET ${formatted.join(
+    ","
+  )} WHERE id = '${id}'`;
+  pgdb(async (client) => {
+    // eslint-disable-next-line no-use-before-define
+    await new PgAction(update_statement, client).doAction(callback);
+  });
+};
+
+exports.save = insert;
+exports.update = update;
+
+class PgAction {
+  action;
+  client;
+  constructor(action, client) {
+    this.action = action;
+    this.client = client;
+  }
+
+  async doAction(callback) {
+    try {
+      const res = await this.client.query(this.action);
+      callback(null, res);
+    } catch (err) {
+      callback(err, null);
+    }
+  }
+}
